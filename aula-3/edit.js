966a034ac45d638068f1384700efeef5420f3cb4
Karol Lima <110504908+karol-lima@users.noreply.github.com>
2023-05-24 15:16:29 -0300

    Add files via upload
    Atividade da 3 aula

---
+++ aula-3/edit.js
@@ -0,0 +1,26 @@
+function add(first, second){
+    return first + second;
+}
+
+function sub(first, second){
+    return first - second;
+}
+
+function mul(first, second){
+    return first*second;
+}
+
+function div(first, second){
+    return first/second;
+}
+
+function is_even(number){
+    if (number%2 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
